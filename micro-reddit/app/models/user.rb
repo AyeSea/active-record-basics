a2d0a5569f12e29c4b45cf9ebac424a06b314c7c
Andrew Chung <andrew.chung159@gmail.com>
2015-03-27 10:03:47 -0400

    Add format validation to email attribute

--- micro-reddit/app/models/user.rb
+++ micro-reddit/app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
+	before_save { email.downcase! }
 	validates :username, presence: true, uniqueness: true,
 					   length: { in: 5..20 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, uniqueness: { case_sensitive: false },
 						 length: { in: 5..35 }
+						 format: { with: VALID_EMAIL_REGEX }
 	validates :password, presence: true, uniqueness: true,
 						 length: { in: 8..30 }
 
